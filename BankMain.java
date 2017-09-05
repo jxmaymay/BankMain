494601a26b6d8f87ffefbfa3759cc11eaa39e0ba
jxmaymay <31222311+jxmaymay@users.noreply.github.com>
2017-09-05 10:44:52 -1000

    Add files via upload

--- BankMain.java
+++ BankMain.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Main bank operation.
@@ -6,18 +6,48 @@ import java.util.ArrayList;
  */
 
 public class BankMain {
+
 	public static void main(String[] args) {
-		ArrayList<Object> accounts = new ArrayList<Object>();
-		
 		SavingsAccount sa1 = new SavingsAccount(1, 0.0);
-		accounts.add(sa1);
-		//initialize new savings account + add to registry
+		String in;
+		double param;
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Welcome to the bank terminal.");
+		System.out.println("----------------");
 		
-		sa1.deposit(101.25);
-		
-		sa1.deposit(87.68);
-		sa1.withdraw(62.93);
-		
-		
+		boolean q = false;
+		while (q == false) {
+			System.out.println("W)ithdraw   D)eposit   M)onth   Q)uit");
+			System.out.println("");
+			in = scnr.next();
+			if (in.equals("W") || in.equals("w")) {
+				System.out.println("How much to withdraw? ");
+				System.out.println("");
+				param = scnr.nextDouble();
+				sa1.withdraw(param);
+			}
+			
+			if (in.equals("D") || in.equals("d")) {
+				System.out.println("How much to deposit? ");
+				System.out.println("");
+				param = scnr.nextDouble();
+				sa1.deposit(param);
+			}
+			
+			if (in.equals("Q") || in.equals("q")) {
+				System.out.println("ENDING SESSION");
+				q = true;
+			
+			if (in.equals("M") || in.equals("m")) {
+				System.out.println("Ending account month... ");
+				System.out.println("");
+				sa1.monthEnd();
+			}
+		}
+		//ArrayList<Account> accountList = new ArrayList<Account>();
+		//for (Account personInArrayList: accountList) {
+			//System.out.println(personInArrayList.getBalance() + "\n");
+	//}
 	}
+  }
 }
--- SavingsAccount.java
+++ SavingsAccount.java
@@ -3,16 +3,32 @@
  * @author Mason Jenkins
  */
 
+
 public class SavingsAccount extends Account {
 	private static final double interestRate = 0.0314; 
 	//3.14 percent interest rate
+	private int freeWithdraws = 0;
 	
 	public SavingsAccount(int aNum, double bal) {
 		super(aNum, bal);
 	}
 	
+	public void withdraw(double amt) {
+		setBalance(getBalance() - amt);
+		System.out.println("ACCOUNT " + getAccNum()
+				+ "\n -- BALANCE: " + getBalance());
+		freeWithdraws++;
+		if (freeWithdraws > 3) {
+			setBalance(getBalance() - 1);
+			System.out.println("Account#" + getAccNum() + " charged processing fee.");
+		}
+	}
+	
 	public void monthEnd() {
+		freeWithdraws = 0;
 		setBalance(getBalance() + (getBalance() * interestRate));
+		setBalance(Math.round(getBalance() * 100));
+		setBalance(getBalance() / 100);
 		displayBal();
 	}
 }
